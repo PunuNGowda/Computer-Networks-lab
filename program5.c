84b04d3adde9e3da6fcf899bf60e47b418226b62
Punu N Gowda <127032735+PunuNGowda@users.noreply.github.com>
2023-05-15 14:56:22 +0530

    Update program5.c

--- program5.c
+++ program5.c
@@ -1,64 +1,81 @@
-
-#include<stdio.h>
 #include<string.h>
+#include<stdio.h>
 
-#define N strlen(gen_poly)
-
-char data[28];
-char check_value[28];
-char gen_poly[10];
-int data_length,i,j;
-
-void XOR(){
-    for(j = 1;j < N; j++)
-    check_value[j] = (( check_value[j] == gen_poly[j])?'0':'1');
+#define N strlen(g)
+char t[28],cs[28],g[28];
+int a,e,c,b;
+void xor()
+{
+    for(c=1;c<N;c++)
+        cs[c]=((cs[c]==g[c])?'0':'1');
 }
-
-void receiver(){
-    printf("Enter the received data: ");
-    scanf("%s", data);
-    printf("\n-----------------------------\n");
-    printf("Data received: %s", data);
-    crc();
-    for(i=0;(i<N-1) && (check_value[i]!='1');i++);
-        if(i<N-1)
-            printf("\nError detected\n\n");
-        else
-            printf("\nNo error detected\n\n");
-}
-
-void crc(){
-    for(i=0;i<N;i++)
-        check_value[i]=data[i];
-    do{
-        if(check_value[0]=='1')
-            XOR();
-        for(j=0;j<N-1;j++)
-            check_value[j]=check_value[j+1];
-        check_value[j]=data[i++];
-    }while(i<=data_length+N-1);
-}
+void crc()
+{
+    for(e=0;e<N;e++)
+        cs[e]=t[e];
+    do
+    { 
+        if(cs[0]=='1')
+            xor();
+        for(c=0;c<N-1;c++)
+            cs[c]=cs[c+1];
+        cs[c]=t[e++];
+    }while(e<=a+N-1);
+} 
 
 int main()
-{
-    printf("\nEnter data to be transmitted: ");
-    scanf("%s",data);
-    printf("\n Enter the Generating polynomial: ");
-    scanf("%s",gen_poly);
-    data_length=strlen(data);
-    for(i=data_length;i<data_length+N-1;i++)
-        data[i]='0';
-    printf("\n----------------------------------------");
-    printf("\n Data padded with n-1 zeros : %s",data);
-    printf("\n----------------------------------------");
-    crc();
-    printf("\nCRC or Check value is : %s",check_value);
-    for(i=data_length;i<data_length+N-1;i++)
-        data[i]=check_value[i-data_length];
-    printf("\n----------------------------------------");
-    printf("\n Final data to be sent : %s",data);
-    printf("\n----------------------------------------\n");
-
-    receiver();
-        return 0;
-}
+{ 
+    int flag=0;
+    do
+    {
+        printf("\n1.crc12\n2.crc16\ncrc ccit\n4.exit\n\nEnter your option.");
+        scanf("%d",&b);
+        switch(b)
+        { 
+            case 1:strcpy(g,"1101");
+                    break;
+            case 2:strcpy(g,"11000000000000101");
+                    break;
+            case 3:strcpy(g,"10001000000100001");
+                    break;
+            case 4:return 0;
+        }
+        printf("\n enter data:");
+        scanf("%s",t);
+        printf("\n-----------------------\n");
+        printf("\n generating polynomial:%s",g);
+        a=strlen(t);
+        for(e=a;e<a+N-1;e++)
+            t[e]='0';
+        printf("\n--------------------------\n");
+        printf("mod-ified data is:%s",t);
+        printf("\n-----------------------\n");
+        crc();
+        printf("checksum is:%s",cs);
+        for(e=a;e<a+N-1;e++)
+        t[e]=cs[e-a];
+        printf("\n-----------------------\n");
+        printf("\n final codeword is : %s",t);
+        printf("\n------------------------\n");
+        printf("\ntest error detection 0(yes) 1(no)?:");
+        scanf("%d",&e);
+        if(e==0)
+        {
+            do
+            {
+                printf("\n\tenter the position where error is to be inserted:");
+                scanf("%d",&e);
+            }while(e==0||e>a+N-1);
+            t[e-1]=(t[e-1]=='0')?'1':'0';
+            printf("\n-----------------------\n");
+            printf("\n\terroneous data:%s\n",t);
+        } 
+        crc();
+        for(e=0;(e<N-1)&&(cs[e]!='1');e++);
+        if(e<N-1)
+            printf("error detected\n\n");
+        else
+            printf("\n no error detected \n\n");
+        printf("\n-----------------------");
+    }while(flag!=1);
+} 
