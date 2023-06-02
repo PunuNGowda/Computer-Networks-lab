cb17dd52f435607159c587fbfb21d61a2d7a7169
Amit Naik <87256071+AmitNaik01@users.noreply.github.com>
2023-06-02 17:31:07 +0530

    Update program6.c

--- program6.c
+++ program6.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctime>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define ll long int
 
-void transmission(ll & i, ll & N, ll & tf, ll & tt) {
+void transmission(ll i, ll N, ll tf, ll tt) {
   while (i <= tf) {
     int z = 0;
-    for (int k = i; k < i + N && k <= tf; k++) {
-      printf("Sending Frame %d ...\n",k);
+    for (ll k = i; k < i + N && k <= tf; k++) {
+      printf("Sending Frame %li ...\n", k);
       tt++;
     }
-    for (int k = i; k < i + N && k <= tf; k++) {
+    for (ll k = i; k < i + N && k <= tf; k++) {
       int f = rand() % 2;
       if (!f) {
-	printf("Acknowledgment for Frame   %d ...  \n",k);
+        printf("Acknowledgment for Frame %li ...  \n", k);
         z++;
       } else {
-	printf("Timeout!! Frame Number : %d Not Received \n",k);
-	printf("Retransmitting Window... \n");        
+        printf("Timeout!! Frame Number: %li Not Received \n", k);
+        printf("Retransmitting Window... \n");
         break;
       }
     }
@@ -26,15 +27,19 @@ void transmission(ll & i, ll & N, ll & tf, ll & tt) {
     i = i + z;
   }
 }
+
 int main() {
   ll tf, N, tt = 0;
   srand(time(NULL));
-  printf("Enter the Total number of frames : ");
-  scanf("%li",&tf);	  
-  printf("Enter the Window Size : ");
-  scanf("%li",&N);
+
+  printf("Enter the Total number of frames: ");
+  scanf("%li", &tf);
+  printf("Enter the Window Size: ");
+  scanf("%li", &N);
+
   ll i = 1;
   transmission(i, N, tf, tt);
-  printf("Total number of frames which were sent and resent are : %li  \n",tt);
+
+  printf("Total number of frames which were sent and resent are: %li  \n", tt);
   return 0;
 }
